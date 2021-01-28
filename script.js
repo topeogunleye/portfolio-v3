757bd3f6572016c9332fc2f470f620b0e1266f39
Temitope Ogunleye <topeogunleye1@gmail.com>
2021-01-28 04:43:53 +0100

    Increases width of contact form and centers contact form submit button

--- script.js
+++ script.js
@@ -631,13 +631,10 @@ contacts.forEach((contact) => {
       <form class="form-style-9">
         <ul>
         <li>
-            <input type="text" name="field1" class="field-style field-split align-left" placeholder="Name" />
-            <input type="email" name="field2" class="field-style field-split align-right" placeholder="Email" />
-        
+            <input type="text" name="field1" class="field-style field-full align-none" placeholder="Name" />
         </li>
         <li>
-            <input type="text" name="field3" class="field-style field-split align-left" placeholder="Phone" />
-            <input type="url" name="field4" class="field-style field-split align-right" placeholder="Website" />
+            <input type="email" name="field3" class="field-style field-full align-none" placeholder="Email" />
         </li>
         <li>
         <input type="text" name="field3" class="field-style field-full align-none" placeholder="Subject" />
@@ -647,7 +644,9 @@ contacts.forEach((contact) => {
         </li>
         <li>
         <!-- <input class="btn btn-blue download"type="submit" value="Send Message" /> -->
+        <div class="center">
         <a href="" class="btn btn-blue download">Send Message</a>
+        </div>
         </li>
         </ul>
         </form>
